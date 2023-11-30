31fcee296cb40d7f67e0571d25ac0ed738039159
DanYu12 <dan.daniel.yu@gmail.com>
2023-11-30 11:31:09 -0500

    updates

---
+++ src/Pages/Coffee/Coffee.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Coffee = () => {
+  return (
+    <div>
+      <header>
+        <h1>Coffee</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Coffee;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        All rights reserved by Daniel Yu, Cheng Lin, Danielle Hsieh{' '}
+        <a href="">Credits</a> &#169;
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
