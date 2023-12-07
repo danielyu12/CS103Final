f3f5eeb4c0421847d8c4d78f49926fb8dff5ec50
cheng <chengl@bu.edu>
2023-12-06 23:50:27 -0500

    working home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Nav from '../../components/Navbar/Nav';
 import Footer from '../../components/Footer/Footer';
 import Body from '../../components/Body/Body';
-import image1 from '../../assets/home/image1.jpeg';
-import image2 from '../../assets/home/image2.jpeg';
-import image3 from '../../assets/home/image3.jpeg';
-import image4 from '../../assets/home/image4.jpeg';
+import image1 from '../../assets/home/image1.jpg';
+import image2 from '../../assets/home/image2.jpg';
+import image3 from '../../assets/home/image3.jpg';
+import image4 from '../../assets/home/image4.jpg';
 
 const Home = () => {
   const images = [
-    { name: "", source: image1 },
-    { name: "Yume ga Akura", source: image2 },
-    { name: "Yume ga Akura", source: image3 },
-    { name: "Yume ga Akura", source: image4 },
+    { name: "Barcelona", source: image1 },
+    { name: "Carmelina", source: image2 },
+    { name: "Ogawa", source: image3 },
+    { name: "Yume ga Arukara", source: image4 },
   ]
   const text = "Welcome to Boston Bites & Sips, your ultimate guide to the vibrant culinary scene of this historic city! Explore the diverse flavors that Boston has to offer as we curate a delightful collection of restaurants, cozy coffee shops, and lively bars. Whether you're a local looking for hidden gems or a visitor eager to taste the essence of Boston, let your culinary journey begin here.";
   return (
