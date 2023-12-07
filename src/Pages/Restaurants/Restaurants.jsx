e99a74b1942a8b2737b845e921b8b3da62f38e4c
DanYu12 <dan.daniel.yu@gmail.com>
2023-12-06 22:30:44 -0500

    pieronis

--- src/Pages/Restaurants/Restaurants.jsx
+++ src/Pages/Restaurants/Restaurants.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-alice-carousel';
 import Nav from '../../components/Navbar/Nav';
 import './Restaurants.css';
 import Footer from '../../components/Footer/Footer';
+import Pieronis from '../../assets/restaurants/pieronis.jpg';
 
 const Restaurants = () => {
   return (
@@ -16,14 +17,21 @@ const Restaurants = () => {
           ↑ Select one of the links above to see what restaurants Boston has to
           offer! ↑
         </h2>
-        <p>
-          Indulge your taste buds in Boston's diverse culinary landscape with
-          our curated selection of restaurants. Whether you're seeking the
-          laid-back charm of casual dining, the refined elegance of fine dining
-          establishments, or the swift and delicious offerings of fast-casual
-          gems, our city guide ensures you experience the full spectrum of
-          dining experiences.
-        </p>
+        <div>
+          <div>
+            <img src={Pieronis} alt="Pieroni's Restaurant" />
+            <p className="caption"></p>
+          </div>
+
+          <p>
+            Indulge your taste buds in Boston's diverse culinary landscape with
+            our curated selection of restaurants. Whether you're seeking the
+            laid-back charm of casual dining, the refined elegance of fine
+            dining establishments, or the swift and delicious offerings of
+            fast-casual gems, our city guide ensures you experience the full
+            spectrum of dining experiences.
+          </p>
+        </div>
       </main>
       <Footer />
     </div>
